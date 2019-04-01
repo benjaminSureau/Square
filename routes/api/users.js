4d712be50083227f01578c1464e6fed810aa666b
ben sureau <sureau117@gmail.com>
2019-04-01 16:09:49 +0200

    feat(routes\api\user): create CRUD functions for user object

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,70 +2,76 @@ var mongoose = require('mongoose');
 var router = require('express').Router();
 var User = mongoose.model('User');
 
-router.get('/:userId', function(req, res, next){
-    User.findById(req.params.id).then(function(user){
-        if(!user){ return res.sendStatus(401); }
+router.post('/', function(req, res, next){
+    var user = new User();
 
+    user.firstName = req.body.firstName;
+    user.lastName = req.body.lastName;
+    user.birthDate = req.body.birthDate;
+    user.cityOfResidence = req.body.cityOfResidence;
+
+    user.save().then(function(){
         return res.json({user: user});
     }).catch(next);
 });
 
-/*router.put('/user', auth.required, function(req, res, next){
-    User.findById(req.payload.id).then(function(user){
-        if(!user){ return res.sendStatus(401); }
-
-        // only update fields that were actually passed...
-        if(typeof req.body.user.username !== 'undefined'){
-            user.username = req.body.user.username;
-        }
-        if(typeof req.body.user.email !== 'undefined'){
-            user.email = req.body.user.email;
-        }
-        if(typeof req.body.user.bio !== 'undefined'){
-            user.bio = req.body.user.bio;
-        }
-        if(typeof req.body.user.image !== 'undefined'){
-            user.image = req.body.user.image;
-        }
-        if(typeof req.body.user.password !== 'undefined'){
-            user.setPassword(req.body.user.password);
-        }
-
-        return user.save().then(function(){
-            return res.json({user: user.toAuthJSON()});
-        });
-    }).catch(next);
-});*/
-
-/*router.post('/users/login', function(req, res, next){
-    if(!req.body.user.email){
-        return res.status(422).json({errors: {email: "can't be blank"}});
+router.get('/:userId', function(req, res, next){
+    if (mongoose.Types.ObjectId.isValid(req.params.userId)) {
+        User.findById(req.params.id).then(function (user) {
+            if (!user) {
+                return res.sendStatus(404);
+            }
+            return res.json({user: user});
+        }).catch(next);
+    } else {
+        return res.sendStatus(400);
     }
+});
 
-    if(!req.body.user.password){
-        return res.status(422).json({errors: {password: "can't be blank"}});
+router.put('/:userId', function(req, res, next){
+    if (mongoose.Types.ObjectId.isValid(req.params.userId)) {
+        User.findById(req.params.userId).then(function (user) {
+            if (!user) {
+                return res.sendStatus(404);
+            }
+
+            // only update fields that were actually passed...
+            if (typeof req.body.firstName !== 'undefined') {
+                user.firstName = req.body.firstName;
+            }
+            if (typeof req.body.lastName !== 'undefined') {
+                user.lastName = req.body.lastName;
+            }
+            if (typeof req.body.birthDate !== 'undefined') {
+                user.birthDate = new Date(req.body.birthDate);
+            }
+            if (typeof req.body.cityOfResidence !== 'undefined') {
+                user.cityOfResidence = req.body.cityOfResidence;
+            }
+            return user.save().then(function () {
+                return res.json({user: user});
+            });
+        }).catch(next);
+    }else {
+        return res.sendStatus(400);
     }
+});
 
-    passport.authenticate('local', {session: false}, function(err, user, info){
-        if(err){ return next(err); }
-
-        if(user){
-            user.token = user.generateJWT();
-            return res.json({user: user.toAuthJSON()});
-        } else {
-            return res.status(422).json(info);
-        }
-    })(req, res, next);
-});*/
-
-router.post('/', function(req, res, next){
-    console.log("ozpdajzdpoaz");
-    var user = new User();
-
-    user.name = req.body.name;
-    user.save().then(function(){
-        return res.json({user: user});
-    }).catch(next);
+router.delete('/:userId', function(req, res, next){
+    if (mongoose.Types.ObjectId.isValid(req.params.userId)) {
+        User.deleteOne({
+            _id: req.params.userId
+        })
+            .then(function (user) {
+                if (!user) {
+                    return res.sendStatus(404);
+                }
+                return res.sendStatus(200);
+            })
+            .catch(next);
+    } else {
+        return res.sendStatus(400);
+    }
 });
 
 module.exports = router;
