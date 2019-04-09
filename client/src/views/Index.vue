40bed9793c320161b9b0db660c0ae6c12a6064df
theymard <thomas.eymard@orange.fr>
2019-04-09 10:09:18 +0200

    feat(vue js): add client project

---
+++ client/src/views/Index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+  <table>
+      <thead>
+        <tr>
+
+           <td>firstName</td>
+           <td>lastName</td>
+           <td>birthDate</td>
+
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="message in messages" :key="message._id">
+            <td>{{message.firstName}}</td>
+            <td>{{message.lastName}}</td>
+            <td>{{message.birthDate}}</td>
+
+        </tr>
+      </tbody>
+    </table>
+    <hr>
+  </div>
+</template>
+
+
+
+<script>
+const API_URL = "http://localhost:4000/api/users/";
+
+export default {
+  name: "home",
+  data: () => ({
+    error: "",
+    messages: []
+  }),
+
+  mounted() {
+    fetch(API_URL)
+      .then(response => response.json())
+      .then(result => {
+        this.messages = result;
+      });
+  },
+  methods: {}
+};
+</script>
+
+
+<style>
+</style>
